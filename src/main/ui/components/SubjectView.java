7c333116052aaeb02048ac039f094c6cf90e83f0
KarasuRou <Tjalf0000@gmx.de>
2021-08-02 11:51:03 +0200

    Created add/changer/remover in SubjectView for the GUI.

--- src/main/ui/components/SubjectView.java
+++ src/main/ui/components/SubjectView.java
@@ -1,30 +1,37 @@
 package ui.components;
 
+import com.sun.istack.internal.Nullable;
 import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundSize;
+import model.Subject;
 
 public class SubjectView {
 
     private final static SubjectView subjectView = new SubjectView();
     private final TabPane root = new TabPane();
+    private final Property<Number> selectedSubject = new SimpleIntegerProperty();
     private final Property<Number> width = new SimpleDoubleProperty();
     private final Property<Number> height = new SimpleDoubleProperty();
     private final QuestionView questionView = QuestionView.getInstance(); // For question content
 
     static {
+        subjectView.selectedSubject.bind(subjectView.root.getSelectionModel().selectedIndexProperty());
+
         subjectView.root.getTabs().addAll(new Tab("Deutsch"),
                 new Tab("Englisch"),
                 new Tab("Mathematik")
         );
-        String content = "";
-        for (int i = 0; i < 10; i++) {
-            content += "Das ist der Text in dem Fach: " + subjectView.root.getTabs().get(0).getText() + "\n";
-        }
         subjectView.root.getTabs().get(0).setContent(subjectView.questionView.getQuestionView());
         subjectView.root.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
             subjectView.root.getTabs().get(oldValue.intValue()).setContent(null);
@@ -79,13 +86,63 @@ public class SubjectView {
         this.root.setTabMinHeight(40);
         this.root.setMinHeight(120);
         this.root.setPrefHeight(this.height.getValue().doubleValue() * 0.9);
-        this.height.addListener((observable, oldValue, newValue) -> {
-            this.root.setPrefHeight((Double) newValue * 0.9);
-        });
+        this.height.addListener((observable, oldValue, newValue) -> this.root.setPrefHeight((Double) newValue * 0.9));
 
-        this.root.setStyle("-fx-border-color: gray;" +
+        this.root.setStyle(
+                "-fx-border-color: gray;" +
                 "-fx-border-style: solid;" +
-                "-fx-border-width: 0.2px 0 0 0.2px;");
+                "-fx-border-width: 0.2px 0 0 0.2px;"
+        );
+    }
+
+    /**
+     * <p>Adds a new Subject Tab, without content.</p>
+     * @param subject required {@link Subject}
+     */
+    public void addSubjectTab(Subject subject){
+        Tab tab = new Tab(subject.getName().getValue());
+        tab.setId(String.valueOf(subject.getID()));
+        if (subject.getBackgroundPicturePath() != null && !subject.getBackgroundPicturePath().equals("")) {
+            changeBackground(subject.getBackgroundPicturePath());
+        }
+        this.root.getTabs().add(tab);
+    }
+
+    /**
+     * <p>Changes the Background for the current Subject.</p>
+     * @param path relativ system path
+     */
+    public void changeBackground(@Nullable String path) {
+        if (path == null) {
+            this.root.setBackground(null);
+        } else {
+            Image image = new Image(path);
+            BackgroundPosition backgroundPosition = new BackgroundPosition(null, 100.0, true, null, 100.0, true);
+            BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+            Background background = new Background(new BackgroundImage(image, null, null, backgroundPosition, backgroundSize));
+            this.root.setBackground(background);
+        }
+    }
+
+    /**
+     * <p>Renames the Subject Tab.</p>
+     * @param subject required {@link Subject}
+     */
+    public void renameSubjectTab(Subject subject){
+        for (Tab tab : this.root.getTabs()) {
+            if (tab.getId().equals(String.valueOf(subject.getID()))) {
+                tab.setText(subject.getName().getValue());
+                return;
+            }
+        }
+    }
+
+    /**
+     * <p>Deletes the Subject Tab.</p>
+     * @param subject required {@link Subject}
+     */
+    public void deleteSubjectTab(Subject subject){
+        this.root.getTabs().removeIf(tab -> tab.getId().equals(String.valueOf(subject.getID())));
     }
 
     /**
