e8babfd43d942353a006f28f05858b63ed3d37c3
KarasuRou <Tjalf0000@gmx.de>
2021-08-06 19:53:46 +0200

    feat(SubjectView): Added a ContextMenu -> Rename, Delete, Change Background
    The Subjects can be renamed, deleted or the background can be changed
    with a right click (ContextMenu) now.

--- src/main/ui/components/SubjectView.java
+++ src/main/ui/components/SubjectView.java
@@ -5,13 +5,22 @@ import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import logic.miscellaneous.Output;
 import model.Subject;
+import ui.MainUI;
 
 import java.io.File;
 
@@ -50,7 +59,7 @@ public class SubjectView {
                         "Der vorherige Text war aus: " + subjectView.root.getTabs().get(oldValue.intValue()).getText() + "\n\n";
             }
             subjectView.root.getTabs().get(newValue.intValue()).setContent(new Label(content_));
-            subjectView.changeBackground((String) subjectView.root.getTabs().get(newValue.intValue()).getUserData());
+            subjectView.changeCurrentBackground((String) subjectView.root.getTabs().get(newValue.intValue()).getUserData());
             System.out.println("From: " +subjectView.root.getTabs().get(oldValue.intValue()).getText() + " (" + oldValue + ") " +
                     "to: " +subjectView.root.getTabs().get(newValue.intValue()).getText() + " (" + newValue + ")");
         });
@@ -119,14 +128,16 @@ public class SubjectView {
 //        if (subject.getBackgroundPicturePath() != null && !subject.getBackgroundPicturePath().equals("")) {
 //            changeBackground(subject.getBackgroundPicturePath()); // TODO setSubject()
 //        }
+        tab.setContextMenu(getTabContextMenu(subject));
         this.root.getTabs().add(tab);
+        System.out.println(this.root.getTabs().indexOf(tab));
     }
 
     /**
      * <p>Changes the Background for the current Subject.</p>
      * @param path relativ system path
      */
-    public void changeBackground(@Nullable String path) {
+    public void changeCurrentBackground(@Nullable String path) {
         if (path == null) {
             this.root.setBackground(null);
         } else {
@@ -135,6 +146,16 @@ public class SubjectView {
             BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
             Background background = new Background(new BackgroundImage(image, null, null, backgroundPosition, backgroundSize));
             this.root.setBackground(background);
+        }
+    }
+
+    public void changeBackgroundFromSubjectTab(Subject subject) {
+        for (Tab tab : this.root.getTabs()) {
+            if (tab.getId().equals(String.valueOf(subject.getID()))) {
+                Output.write("Changing Subject Background from: " + subject.getName().getValue() + " (ID: " + subject.getID() + ")");
+                tab.setUserData(subject.getBackgroundPicturePath());
+                return;
+            }
         }
     }
 
@@ -157,7 +178,13 @@ public class SubjectView {
      * @param subject required {@link Subject}
      */
     public void deleteSubjectTab(Subject subject){
-        this.root.getTabs().removeIf(tab -> tab.getId().equals(String.valueOf(subject.getID())));
+        for (Tab tab : this.root.getTabs()) {
+            if (tab.getId().equals(String.valueOf(subject.getID()))) {
+                Output.write("Delete Subject: " + subject.getName().getValue() + " (ID: " + subject.getID() + ")");
+                this.root.getTabs().remove(tab);
+                return;
+            }
+        }
     }
 
     /**
@@ -174,5 +201,131 @@ public class SubjectView {
      */
     public Node getSubjectView(){
         return this.root;
+    }
+
+    private ContextMenu getTabContextMenu(Subject subject) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem rename = new MenuItem("Umbenennen");
+        rename.setOnAction(event -> getRenamePopUp(subject));
+        MenuItem changeBackground = new MenuItem("Hintergrund ändern");
+        changeBackground.setOnAction(event -> getChangeBackgroundPopUp(subject));
+        MenuItem delete = new MenuItem("Löschen");
+        delete.setOnAction(event -> getDeletePopUp(subject));
+
+        contextMenu.getItems().addAll(rename, changeBackground, delete);
+        return contextMenu;
+    }
+
+    private void getRenamePopUp(Subject subject) {
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(20));
+        Stage stage = getPopUpStage(vBox);
+        stage.setTitle("Fach(Tab) umbenennen");
+
+        Label label = new Label("Wie soll das Fach(Tab) heißen?");
+        TextField textField = new TextField();
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        Button renameButton = new Button("Umbenennen");
+        renameButton.setOnAction(event -> {
+            if (!textField.getText().equals("")) {
+                subject.setName(textField.getText());
+                renameSubjectTab(subject);
+                stage.close();
+            } else {
+                textField.setStyle("-fx-border-color: red;");
+                Tooltip tooltip = new Tooltip("Bitte Namen angeben!");
+                tooltip.setAutoHide(false);
+                textField.setTooltip(tooltip);
+                tooltip.show(stage);
+            }
+        });
+        textField.setOnKeyReleased(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                renameButton.fire();
+            }
+        });
+        Button cancelButton = new Button("Abbrechen");
+        cancelButton.setOnAction(event -> stage.close());
+        hBox.getChildren().addAll(renameButton, cancelButton);
+
+
+        vBox.getChildren().addAll(label, textField, hBox);
+        stage.show();
+    }
+
+    private void getChangeBackgroundPopUp(Subject subject) {
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(20));
+        Stage stage = getPopUpStage(vBox);
+        stage.setTitle("Fach(Tab) hintergrund ändern");
+
+        Label label = new Label("Hintergrundbild entfernen oder neues Hintergrundbild auswählen?");
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        Button deleteBackground = new Button("Entfernen");
+        deleteBackground.setOnAction(event -> {
+            subject.setBackgroundPicturePath(null);
+            changeBackgroundFromSubjectTab(subject);
+            stage.close();
+        });
+        Button addNewBackground = new Button("Neuen Hintergrund auswählen");
+        addNewBackground.setOnAction(event -> {
+
+            FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                subject.setBackgroundPicturePath(file.getPath());
+                changeBackgroundFromSubjectTab(subject);
+                stage.close();
+            }
+        });
+        hBox.getChildren().addAll(deleteBackground, addNewBackground);
+
+
+        vBox.getChildren().addAll(label, hBox);
+        stage.show();
+    }
+
+    private void getDeletePopUp(Subject subject) {
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(20));
+        Stage stage = getPopUpStage(vBox);
+        stage.setTitle("Fach(Tab) löschen");
+
+        Label label = new Label("Fach(Tab) \"" + subject.getName().getValue() + "\" wirklich löschen?");
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+
+        Button deleteButton = new Button("Löschen");
+        deleteButton.setOnAction(event -> {
+            deleteSubjectTab(subject);
+            stage.close();
+        });
+        Button cancelButton = new Button("Abbrechen");
+        cancelButton.setOnAction(event -> stage.close());
+
+        hBox.getChildren().addAll(deleteButton, cancelButton);
+
+        vBox.getChildren().addAll(label, hBox);
+        stage.show();
+    }
+
+    private Stage getPopUpStage(Parent root) {
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setResizable(false);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        MainUI.getInstance().setInitOwner(stage);
+        return stage;
     }
 }
